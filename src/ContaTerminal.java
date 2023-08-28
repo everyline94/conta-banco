e849f438b56a0adc19a46f0cb1af0ff40cb04b82
bruno larocca <brunolujanlarocca@gmail.com>
2023-08-28 14:01:15 -0300

    implementação da lógica proposta no exercício no arquivo ContaTerminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Bem vindo!");
+
+        System.out.println("Por favor, digite o número da sua Conta:");
+        int numero = sc.nextInt();
+        System.out.println("Por favor, digite o número da sua Agência: ");
+        String agencia = sc.next();
+        System.out.println("Por favor, digite o seu Nome:");
+        String nomeCliente = sc.next();
+        double saldo = 237.48;
+
+        System.out.println("Olá, " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agencia é " + agencia + " conta " + numero + " e seu saldo " + saldo +  " já está disponível para saque");
+
+        sc.close();
+
     }
 }
